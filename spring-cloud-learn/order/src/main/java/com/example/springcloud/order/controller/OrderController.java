d811bd0419a8728aa9b712e2845ecff42a006dd5
zhang ying <zhangyingmark@gmail>
2023-05-10 01:20:14 +0800

    feat: nacos discovery client

--- spring-cloud-learn/order/src/main/java/com/example/springcloud/order/controller/OrderController.java
+++ spring-cloud-learn/order/src/main/java/com/example/springcloud/order/controller/OrderController.java
@@ -1,12 +1,23 @@
 package com.example.springcloud.order.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 public class OrderController {
-    @PostMapping("/orders")
+    private final RestTemplate restTemplate;
+
+    @GetMapping("/orders")
     public String create() {
+        log.info("start order...");
+        String url = "http://stock/stocks/reduce";
+        restTemplate.getForObject(url, String.class);
         return "create order success!";
     }
 }
--- spring-cloud-learn/stock/src/main/java/com/example/springcloud/stock/controller/StockController.java
+++ spring-cloud-learn/stock/src/main/java/com/example/springcloud/stock/controller/StockController.java
@@ -1,13 +1,19 @@
 package com.example.springcloud.stock.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 public class StockController {
 
-    @PostMapping("/stocks/reduce")
+    @GetMapping("/stocks/reduce")
     public String reduce() {
+        log.info("库存-1");
         return "扣减库存成功!";
     }
 }
