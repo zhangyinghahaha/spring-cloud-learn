473d551ae4059d3a580555a1fd74a4e819be0a4d
ying.zhang01 <ying.zhang01@united-imaging.com>
2023-07-03 18:05:24 +0800

    feat: nacos config集成

--- spring-cloud-learn/order/src/main/java/com/example/springcloud/order/OrderApplication.java
+++ spring-cloud-learn/order/src/main/java/com/example/springcloud/order/OrderApplication.java
@@ -1,8 +1,11 @@
 package com.example.springcloud.order;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author ying.zhang01
@@ -10,7 +13,15 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class OrderApplication {
+    @Value("${user.name}")
+    private String username;
+
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.printf("[init] user name: %s", username);
     }
 }
