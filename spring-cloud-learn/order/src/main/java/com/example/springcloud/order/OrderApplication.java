6180889c270dc4fcb5169de83ae3f4f479fc965a
ying.zhang01 <ying.zhang01@united-imaging.com>
2023-07-13 22:39:32 +0800

    feat: nacos config集成

--- spring-cloud-learn/order/src/main/java/com/example/springcloud/order/OrderApplication.java
+++ spring-cloud-learn/order/src/main/java/com/example/springcloud/order/OrderApplication.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author ying.zhang01
@@ -16,12 +18,21 @@ public class OrderApplication {
     @Value("${user.name}")
     private String username;
 
-    public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(OrderApplication.class, args);
+
+        while(true) {
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+            //获取当前部署的环境
+            String currentEnv = applicationContext.getEnvironment().getProperty("current.env");
+            System.err.println("in "+currentEnv+" enviroment; "+"user name :" + userName + "; age: " + userAge);
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 
     @PostConstruct
     public void init() {
-        System.out.printf("[init] user name: %s", username);
+        System.out.println("user name: " + username);
     }
 }
