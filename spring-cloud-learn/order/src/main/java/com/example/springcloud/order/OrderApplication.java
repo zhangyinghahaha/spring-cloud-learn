81c3e5c1bb5f22867b68c7aeed74ffb86b47b920
zhang ying <zhangyingmark@gmail>
2023-07-14 00:56:27 +0800

    feat: nacos config动态刷新

--- spring-cloud-learn/order/src/main/java/com/example/springcloud/order/OrderApplication.java
+++ spring-cloud-learn/order/src/main/java/com/example/springcloud/order/OrderApplication.java
@@ -3,6 +3,7 @@ package com.example.springcloud.order;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -15,24 +16,16 @@ import java.util.concurrent.TimeUnit;
 @SpringBootApplication
 @EnableFeignClients
 public class OrderApplication {
-    @Value("${user.name}")
-    private String username;
-
     public static void main(String[] args) throws InterruptedException {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(OrderApplication.class, args);
 
-        while(true) {
-            String userName = applicationContext.getEnvironment().getProperty("user.name");
-            String userAge = applicationContext.getEnvironment().getProperty("user.age");
-            //获取当前部署的环境
-            String currentEnv = applicationContext.getEnvironment().getProperty("current.env");
-            System.err.println("in "+currentEnv+" enviroment; "+"user name :" + userName + "; age: " + userAge);
-            TimeUnit.SECONDS.sleep(1);
-        }
-    }
-
-    @PostConstruct
-    public void init() {
-        System.out.println("user name: " + username);
+//        while(true) {
+//            String userName = applicationContext.getEnvironment().getProperty("test.name");
+//            String userAge = applicationContext.getEnvironment().getProperty("test.age");
+//            //获取当前部署的环境
+//            String currentEnv = applicationContext.getEnvironment().getProperty("current.env");
+//            System.err.println("in "+currentEnv+" enviroment; "+"user name :" + userName + "; age: " + userAge);
+//            TimeUnit.SECONDS.sleep(1);
+//        }
     }
 }
--- spring-cloud-learn/order/src/main/java/com/example/springcloud/order/controller/OrderController.java
+++ spring-cloud-learn/order/src/main/java/com/example/springcloud/order/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.example.springcloud.order.controller;
 import com.example.springcloud.order.feign.StockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +13,21 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RefreshScope
 public class OrderController {
     private final StockService stockService;
+    @Value("${test.name}")
+    private String username;
 
     @GetMapping("/orders")
     public String create() {
         log.info("start create order...");
         String stockResult = stockService.get("牙刷");
         return "create order success!\n" + stockResult;
+    }
+
+    @GetMapping("/name")
+    public String name() {
+        return username;
     }
 }
